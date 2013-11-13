011ec2a006b7f670ef17ad3ec0ad2980babe7cab
unknown <j.friedmann@CMAS-FRIEDMANN.perficient.com>
2013-11-13 12:49:31 -0500

    fixes js bug

--- wp-content/themes/JBlog/js/scripts.js
+++ wp-content/themes/JBlog/js/scripts.js
@@ -30,7 +30,7 @@ jQuery(function() {
 				var newdiv = jQuery("<div style='display:none'>"+ data + "</div>");
 				jQuery('#jboil-main-message').append(newdiv);
 				jQuery(newdiv).fadeIn("slow");
-				jQuery(data).find(".jboil-imgliquid").css("visibility", "visible").hide().fadeIn("slow");
+				jQuery(".jboil-imgliquid").filter(function (index) { return jQuery(this).css('visibility') == 'hidden' }).css("visibility", "visible").hide().fadeIn("slow");
 				if (jQuery(data).filter(".jboil-post-block").length < 9) {
 					jQuery("#jboil-load-more").hide();
 				}
