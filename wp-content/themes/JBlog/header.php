f38819d04c86c37bb604eab270a8a04bb29afb53
unknown <j.friedmann@CMAS-FRIEDMANN.perficient.com>
2013-11-13 13:08:39 -0500

    Changes viewport settings so that displays properly on mobile devices

--- wp-content/themes/JBlog/header.php
+++ wp-content/themes/JBlog/header.php
@@ -12,7 +12,7 @@
 	<!-- Basic indentifying Meta Tags -->
 	<meta name="resource-type" content="document">
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php
