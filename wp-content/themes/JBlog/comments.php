566c04fbed13011cbd86f9a09dc45c1f46a45843
Jacob Friedmann <jdf3ha@virginia.edu>
2013-12-06 10:36:26 -0500

    fixes #13

--- wp-content/themes/JBlog/comments.php
+++ wp-content/themes/JBlog/comments.php
@@ -3,7 +3,7 @@
 	$form_args = jboil_comment_args();
 	$list_args = '';
 	echo "<ul class='jboil-comment-list' >";
-		wp_list_comments($list_args, get_comments(array('post_id'=>get_the_ID(),'status'=>'approve'));
+		wp_list_comments($list_args, get_comments(array('post_id'=>get_the_ID(),'status'=>'approve')));
 	echo "</ul>";
 	comment_form($form_args);
 	
